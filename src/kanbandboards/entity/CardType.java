9d368ba88b7c7aa60d4ecf22278b7dca7a72903b
Yash Dave <mail2ypd@gmail.com>
2019-09-16 01:36:23 +0530

    Updates CardType entity

--- src/kanbandboards/entity/CardType.java
+++ src/kanbandboards/entity/CardType.java
@@ -5,6 +5,8 @@
  */
 package kanbandboards.entity;
 
+import java.util.Objects;
+
 /**
  *
  * @author Yash
@@ -13,4 +15,73 @@ public class CardType {
     private int cardTypeId;
     private String cardTypeName;
     private String cardTypeColor;
+
+    public CardType() {
+    }
+
+    public CardType(int cardTypeId, String cardTypeName, String cardTypeColor) {
+        this.cardTypeId = cardTypeId;
+        this.cardTypeName = cardTypeName;
+        this.cardTypeColor = cardTypeColor;
+    }
+
+    public int getCardTypeId() {
+        return cardTypeId;
+    }
+
+    public String getCardTypeName() {
+        return cardTypeName;
+    }
+
+    public String getCardTypeColor() {
+        return cardTypeColor;
+    }
+
+    public void setCardTypeName(String cardTypeName) {
+        this.cardTypeName = cardTypeName;
+    }
+
+    public void setCardTypeColor(String cardTypeColor) {
+        this.cardTypeColor = cardTypeColor;
+    }
+
+    @Override
+    public String toString() {
+        return "CardType{" + "cardTypeId=" + cardTypeId + ", cardTypeName=" + cardTypeName + ", cardTypeColor=" + cardTypeColor + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 23 * hash + this.cardTypeId;
+        hash = 23 * hash + Objects.hashCode(this.cardTypeName);
+        hash = 23 * hash + Objects.hashCode(this.cardTypeColor);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final CardType other = (CardType) obj;
+        if (this.cardTypeId != other.cardTypeId) {
+            return false;
+        }
+        if (!Objects.equals(this.cardTypeName, other.cardTypeName)) {
+            return false;
+        }
+        if (!Objects.equals(this.cardTypeColor, other.cardTypeColor)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
 }
